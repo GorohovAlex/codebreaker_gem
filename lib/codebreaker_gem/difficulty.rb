c83db61b3fc9cbeff5108e97d454a1675f7e71fd
GorohovAlex <gorochov.as@gmail.com>
2019-12-17 14:42:03 +0200

    add module to Difficulty

--- lib/codebreaker_gem/difficulty.rb
+++ lib/codebreaker_gem/difficulty.rb
@@ -1,10 +1,12 @@
-class Difficulty
-  attr_reader :name, :attempts, :hints
+module CodebreakerGem
+  class Difficulty
+    attr_reader :name, :attempts, :hints
 
-  def initialize(name:, attempts:, hints:, level:)
-    @name = name
-    @attempts = attempts
-    @hints = hints
-    @level = level
+    def initialize(name:, attempts:, hints:, level:)
+      @name = name
+      @attempts = attempts
+      @hints = hints
+      @level = level
+    end
   end
 end
