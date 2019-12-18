f3a1ee246d9a5201cc60b5e9db2af2d4a2433e5e
GorohovAlex <gorochov.as@gmail.com>
2019-12-18 12:40:12 +0200

    add statistic

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,6 +1,7 @@
 module Codebreaker
   # require 'pry'
   require 'codebreaker/version'
+  require 'codebreaker/yaml_file.rb'
   require 'codebreaker/validator.rb'
   require 'codebreaker/base_class.rb'
   require 'codebreaker/difficulty.rb'
@@ -8,4 +9,5 @@ module Codebreaker
   require 'codebreaker/game.rb'
   require 'codebreaker/game_stage.rb'
   require 'codebreaker/user.rb'
+  require 'codebreaker/statistic.rb'
 end
---
+++ lib/codebreaker/statistic.rb
@@ -0,0 +1,48 @@
+module Codebreaker
+  class Statistic
+    include YamlFile
+
+    attr_reader :statistic, :headings
+    def initialize
+      @headings = STAT_HEADER_LIST
+      @statistic_items = []
+    end
+
+    def statistic_get
+      statistic_load
+      statistic_sort
+      @statistic_items.each_with_index.map do |stat, index|
+        [index.next, stat[:name], stat[:difficulty], stat[:attempts_total],
+         stat[:attempts_used], stat[:hints_total], stat[:hints_used]]
+      end
+    end
+
+    def statistic_add_item(name:, difficulty:, game_stage:)
+      @statistic_items << { name: name,
+                            difficulty: difficulty.name,
+                            attempts_total: game_stage.attempts,
+                            attempts_used: game_stage.step_number,
+                            hints_total: difficulty.hints,
+                            hints_used: game_stage.hint_used }
+    end
+
+    def statistic_save
+      YamlFile.save(STAT_FILE_PATH, @statistic_items)
+    end
+
+    private
+
+    def statistic_load
+      return unless File.file?(STAT_FILE_PATH) && File.size(STAT_FILE_PATH).positive?
+
+      @statistic_items = YamlFile.load(STAT_FILE_PATH)
+    end
+
+    def statistic_sort
+      @statistic_items.sort_by! do |stat|
+        [stat[:attempts_total] + stat[:hints_total],
+         stat[:attempts_used], stat[:hints_used]]
+      end
+    end
+  end
+end
