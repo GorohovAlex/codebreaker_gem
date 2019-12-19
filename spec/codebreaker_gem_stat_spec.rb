0067d24829ce600814bbeaed32428f2939f3d88e
GorohovAlex <gorochov.as@gmail.com>
2019-12-19 13:56:59 +0200

    fix but with tmp folder

--- spec/codebreaker_gem_stat_spec.rb
+++ spec/codebreaker_gem_stat_spec.rb
@@ -4,10 +4,14 @@ module Codebreaker
     let(:difficulty_change) { Difficulty.new(name: 'Easy', attempts: 15, hints: 2, level: 0) }
     let(:game_stage) { GameStage.new(user_code_length: 4, attempts: 10) }
     let(:user) { instance_double('User', username: 'Smile') }
+    let(:statistic) { Statistic.new }
+    let(:test_file) { 'tmp/test-results/statistic.yml' }
 
     context 'when statistic save' do
       it 'success statistic save' do
         codebreaker.instance_variable_set(:@user, user)
+        stub_const('Codebreaker::Statistic::STAT_FILE_PATH', test_file)
+        codebreaker.instance_variable_set(:@statistic, statistic)
         codebreaker.instance_variable_set(:@difficulty_change, difficulty_change)
         codebreaker.instance_variable_set(:@game_stage, game_stage)
         codebreaker.statistic_save
